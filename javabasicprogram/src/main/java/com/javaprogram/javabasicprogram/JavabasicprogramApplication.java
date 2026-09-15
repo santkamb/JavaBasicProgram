1ce569109334e269032f92f2c353ef3e5dad2fa7
santkamb <santoshbkamble1606@gmail.com>
2026-09-15 16:20:04 +0530

    Initial Commit : Java Basic Program

---
+++ javabasicprogram/src/main/java/com/javaprogram/javabasicprogram/JavabasicprogramApplication.java
@@ -0,0 +1,13 @@
+package com.javaprogram.javabasicprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavabasicprogramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavabasicprogramApplication.class, args);
+	}
+
+}
---
+++ javabasicprogram/src/test/java/com/javaprogram/javabasicprogram/JavabasicprogramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaprogram.javabasicprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavabasicprogramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
